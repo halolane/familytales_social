d5e90e69ca3e5d92291bc04a63641ffe963124a3
halolane <myhalolane@gmail.com>
2013-09-17 21:24:11 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilytalesSocial::Application.config.secret_token = '2fed1a6c4fec9d1c104f57639303a63bf297c37c9d2d50f15e47b7e19510fe5645441d2871188c8b687dcd7171437aa85d4dc2e818180e32f8bd2570174a551f'
